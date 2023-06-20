7996c19e69a8f3701ec111aa6096def99aef19bf
kayo <kayohramos@gmail.com>
2023-06-19 21:37:29 -0300

    feat: req-3, Mapeamento Pratos <> Ingredientes

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,30 @@
 # Req 3
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        dish_menu = {}
+
+        with open(source_path, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                dish_name = row[0]
+                dish_price = float(row[1])
+                ingredient_name = row[2]
+                recipe_amount = int(row[3])
+
+                if dish_name not in dish_menu:
+                    dish_menu[dish_name] = Dish(dish_name, dish_price)
+
+                ingredient = Ingredient(ingredient_name)
+                dish_menu[dish_name].add_ingredient_dependency(
+                    ingredient, recipe_amount
+                )
+
+        self.dishes.update(dish_menu.values())
